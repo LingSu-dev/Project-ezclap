7388a5f083bed3b052725de817b238063b1e9dc4
Ling Su <26560603+flyingzambie@users.noreply.github.com>
2023-05-08 16:54:40 -0400

    added all code and docs

---
+++ doc/sprint0/done.md
@@ -0,0 +1,4 @@
+# Definition of Done
+A feature is considered finished when:
+* It passes a set of working and proper unit tests  
+* Its pull request is reviewed by at least one other member  
---
+++ src/react/src/components/createAccount/logout.component.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Logout extends Component {
+
+    componentDidMount(){
+        localStorage.removeItem('token')
+        localStorage.removeItem('username')
+    }
+
+ 
+  render() {
+
+    return (
+          <div className="App">
+            
+           <p>You are logged out!</p>
+
+          </div>
+    );
+  }
+
+  
+}
+
+export default Logout;
